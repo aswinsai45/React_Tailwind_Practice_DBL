75a20d53cf86e93f10d6efef805294f6120c3a13
aswinsai45 <aswinsai45@gmail.com>
2024-04-19 10:28:06 +0530

    completed Navbar

--- my-project/src/components/Navbar_New.jsx
+++ my-project/src/components/Navbar_New.jsx
@@ -8,13 +8,15 @@ import {FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube} from 'react-
 const Navbar_New = () => {
 
   const [nav, setNav] = useState(false)
+  const [logo, setLogo] = useState(false)
   const handleNav = () =>(
-    setNav(!nav)
-  )
+    setNav(!nav),
+    setLogo(!logo)
+  );
   return (
     <div className='flex justify-between items-center h-20 px-4'>
         <div>
-          <h1>BEACHES.</h1>
+          <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'} >BEACHES.</h1>
         </div>
         <ul className='hidden md:flex'>
           <li>Home</li>
@@ -24,18 +26,20 @@ const Navbar_New = () => {
           <li>Book</li>
         </ul>
         <div className='hidden md:flex'>
-        <BiSearch size = {20}/>
+        <BiSearch className='mr-4' size = {20}/>
         <BsPerson size = {20}/>
         </div>
         
-        <div onClick={handleNav} className='md:hidden'> {/*hamburger menu for devices less than md width*/}
+        {/*hamburger nav action */}
+
+        <div onClick={handleNav} className='md:hidden z-10'> {/*hamburger menu for devices less than md width*/}
               {nav ? <AiOutlineClose size={20} /> : <HiOutlineMenuAlt4 size={20} />}
-            <HiOutlineMenuAlt4 size = {20}/>
+           
         </div>
 
         {/*dropdown div : MOBILE ONLY*/}
 
-        <div className='absolute left-[-100%] top-0 w-full bg-gray-100/90 px-4 py-7 flex-col'>
+        <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
           <ul>
             <h1>BEACHES</h1>
             <li className='border-b'>Home</li>
